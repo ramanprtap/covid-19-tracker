f005bec5c5e0580e5013cce3fe259712e4d1ece8
Raman Pratap <Raman.Pratap@isobar.com>
2020-05-07 03:35:13 +0530

    Updates

---
+++ precache-manifest.b456274b874eb2a5d17d81533f4afc29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6e1615430325f2414d65cd28a44549",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "5564554b58f71b67937c",
+    "url": "/covid-19-tracker/static/css/main.b4fe8043.chunk.css"
+  },
+  {
+    "revision": "7c0704dc0f77e401c299",
+    "url": "/covid-19-tracker/static/js/2.1d3b06c6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.1d3b06c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5564554b58f71b67937c",
+    "url": "/covid-19-tracker/static/js/main.fbd9bebf.chunk.js"
+  },
+  {
+    "revision": "d8644bfd8316e0b47e7d",
+    "url": "/covid-19-tracker/static/js/runtime-main.300547ea.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
